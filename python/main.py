90c5acfafb111aca6011c3f9a4ec8b205bf3e593
tomroth <t.roth1@outlook.com>
2024-10-17 16:55:30 +0200

    implemented list creation and tests

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import pygame as pg
-import sys
+import argparse
+# import sys
 import datetime
 import random
 
@@ -8,59 +9,72 @@ HEIGHT = 720
 WIDTH = 1280
 MAX_PILLAR_HEIGHT = int((HEIGHT - 20) / 100)
 DEFAULT_LIST_LENGTH = 100
+POSSIBLE_LAGORITHMS = ["bogo", "bogo-sort", ]
 class Pillar():
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, value):
+        self.value = value
         
 class List():
-    def __init__(self, length=DEFAULT_LIST_LENGTH, seed=datetime.datetime.now().timestamp()):
+    def __init__(self, algorithm, length=0, seed=1, start_list=None):
         # check validity of kwargs
         assert type(length) == int
+        assert algorithm in POSSIBLE_LAGORITHMS
         
-        possible_numbers = list(range(length))
-        self.pillars = []
-        
-        # set random seed randomly by default, same seed possibke for same results
-        random.seed(seed)
-        for _ in range(length):
-            # select a random number from the possible numbers left
-            num = random.choice(possible_numbers)
+        # if you dont want a random list but rather a preconfigured one
+        if start_list != None:
+            self.pillars = []
+            for value in start_list:
+                self.pillars.append(Pillar(value=value))
+        else:
+            possible_numbers = list(range(1, length + 1))
+            self.pillars = []
             
-            self.pillars.append(Pillar(value=num))
-            
-            # remove the just added number from the possible numbers so we dont have dubbles
-            possible_numbers.remove(num)
+            # set random seed randomly by default, same seed possibke for same results
+            random.seed(seed)
+            for _ in range(length):
+                # select a random number from the possible numbers left
+                num = random.choice(possible_numbers)
+                
+                self.pillars.append(Pillar(value=num))
+                
+                # remove the just added number from the possible numbers so we dont have dubbles
+                possible_numbers.remove(num)
+    
+    def check_sorted(self):
+        for i in range(len(self.pillars) - 1):
+            if self.pillars[i].value > self.pillars[i + 1].value:
+                return False
+        return True
 
 def main():
     # check for valid usage
-    check_usage(sys.argv)
+    args = check_usage()
+    
+    list = List(length=args["length"], seed=args["seed"], algorithm=args["algorithm"])
     
     # setup pygame
     pg.init()
     pg.display.set_mode((WIDTH, HEIGHT))
 
 
-def check_usage(args):
-    # check for right number of command line arguments
-    if len(args) < 1 or len(args) > 3:
-        sys.exit("usage: python main.py {list length}(optional, default=100) {seed}(optional)")
-    try:
-        global list_length
-        list_length = int(args[1])
-    
-    # if not list length is given -> use default
-    except IndexError:
-        list_length = DEFAULT_LIST_LENGTH
-    # if the list length isnt a int -> exit
-    except TypeError:
-        sys.exit("list length must be an integer")
-        
-    if list_length < 2:
-        sys.exit("list length must ber grater than two")
-        
-    if list_length >= 1000:
-        sys.exit("the maximum list length is 1000")
-    print(f"List length: {list_length}")
+def check_usage():
+    # check for right usage of command line arguments
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+            usage="python main.py --list_length {int} (deafult=100) --seed {float} (default=current timestamp)",
+    )
+    parser.add_argument("-l", "--length", default=DEFAULT_LIST_LENGTH, type=int)
+    parser.add_argument("-s", "--seed", default=datetime.datetime.now().timestamp(), type=float)
+    # for now not required and defaults to bogo
+    parser.add_argument("-a", "--algorithm", 
+                        #required=True,
+                        # add more algorithms later
+                        choices=POSSIBLE_LAGORITHMS,
+                        default="bogo"
+                        )
+    # args is a dict of the key-value pairs from the command-line
+    args = vars(parser.parse_args())
+    return args
 
 if __name__=="__main__":
     main()
---
+++ python/tests.py
@@ -0,0 +1,29 @@
+import pytest
+import main as m
+
+SORTED_LIST = [1, 2, 3, 4, 5]
+SEMI_RANDOM_LIST = [2, 4, 3, 5, 1]
+def main():
+    test_list_creation()
+    test_check_sorted()
+    
+def test_list_creation():
+    # test list creation with a list of length 1 
+    list = m.List(length=1, seed=1, algorithm="bogo")
+    assert list.pillars[0].value == 1
+    
+    # test list creation with fixed list:
+    list = m.List(length=len(SEMI_RANDOM_LIST), algorithm="bogo", start_list=SEMI_RANDOM_LIST)
+    for i in range(len(SEMI_RANDOM_LIST)):
+        assert list.pillars[i].value == SEMI_RANDOM_LIST[i]
+
+def test_check_sorted():
+    # test ckheck sorted function
+    list = m.List(length=len(SORTED_LIST), algorithm="bogo", start_list=SORTED_LIST)
+    assert list.check_sorted() == True
+    
+    list = m.List(length=len(SEMI_RANDOM_LIST), algorithm="bogo", start_list=SEMI_RANDOM_LIST)
+    assert list.check_sorted() == False
+    
+if __name__ == "__main__":
+    main()
